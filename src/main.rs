40b61ea5c911b1393a4c79462017ae80aa224988
niketic95 <niketic95@gmail.com>
2022-09-22 03:25:45 +0200

    Initial crosscompile setup
    - Add config for ARMv7-M thumb instruction set
    - Disable std. lib.
    - Setup panic handler to hang the board

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+
+#[panic_handler]
+fn halt_handler(_info: &PanicInfo) -> !{
+    loop{
+
+    };
 }
