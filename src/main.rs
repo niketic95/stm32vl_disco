8736c1e2a50ef212941bd43ff7543fe471a1eacd
niketic95 <niketic95@gmail.com>
2022-09-25 16:03:54 +0200

    Add cortex-m and cortex-m-rt to handle SBP,PC, and reset_vector Pass link.x from cortex-m-rt to lld Add nop to main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 #![no_std]
 #![no_main]
 
+use core::arch::asm;
 use core::panic::PanicInfo;
-
+use cortex_m_rt::entry;
 
 #[panic_handler]
-fn halt_handler(_info: &PanicInfo) -> !{
-    loop{
+fn halt_handler(_info: &PanicInfo) -> ! {
+    loop {}
+}
 
-    };
+#[entry]
+fn main() -> ! {
+    loop {
+        unsafe {
+            asm!("nop");
+        }
+    }
 }
